50e1ecd621a9e1c5d2dc7d570a23fec4d4935eda
GrentsAA <127672810+hudwizzx@users.noreply.github.com>
2024-06-10 14:25:55 +0700

    Add files via upload

---
+++ matrix/ConsoleAppTesting/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly:InternalsVisibleTo("ConsoleInputOutputHandler")]
+namespace ConsoleAppTesting
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            
+
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            int a;
+            int b;
+
+            if (!int.TryParse(input1, out a) || !int.TryParse(input2, out b) || (a <= 0) || (b <= 0))
+            {
+                Console.WriteLine("некорректный ввод");
+
+            }
+
+
+            else
+            {
+                int[,] mass = new int[a, b];
+                for (int i = 0; i < a; i++)
+                {
+                    
+                    for (int j = 0; j < b; j++)
+                    {
+
+                        mass[i,j] = (i + 1) * (j + 1);
+                    }
+                    
+                }
+
+                for (int i = 0; i < a; i++)
+                {
+                    Console.WriteLine();
+                    for (int j = 0; j < b; j++)
+                    {
+
+                        Console.Write(mass[i, j] + " ");
+                    }
+
+                }
+            }
+        }
+    }
+}
